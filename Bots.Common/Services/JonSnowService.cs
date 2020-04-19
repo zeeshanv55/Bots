418d0ee10dd32045d6bddf322161621a667359d8
Zeeshan Vohra <zeesvo@microsoft.com>
2020-04-20 00:24:10 +0530

    Added memory

--- Bots.Common/Services/JonSnowService.cs
+++ Bots.Common/Services/JonSnowService.cs
@@ -15,36 +15,96 @@
 
         private readonly IConfiguration configuration;
 
+        private string[] responses;
+
+        private int responsesSaved = 0;
+
         private static readonly Random random = new Random();
+
+        private static readonly int responsesQueueCapacity = 5;
+
+        private static readonly double logScoreFactor = 2;
 
         public JonSnowService(IConfiguration configuration, IHttpService httpService)
         {
             this.configuration = configuration;
             this.httpService = httpService;
+            this.responses = new string[responsesQueueCapacity];
         }
 
         public async Task<string> GetResponse(string prompt)
         {
+            string response;
+
+            do
+            {
+                response = await this.GetRawResponse(prompt);
+            }
+            while (this.GetResponseScore(response) > 0.67);
+            
+            if (responsesSaved == responsesQueueCapacity)
+            {
+                this.responses = this.responses.Skip(1).Concat(this.responses.Take(1)).ToArray();
+                responsesSaved--;
+            }
+
+            this.responses[responsesSaved] = response;
+            responsesSaved++;
+
+            return response;
+        }
+
+        private double GetResponseScore(string response)
+        {
+            if (!this.responses.Contains(response))
+            {
+                return 1.0;
+            }
+
+            var score = 0.0;
+            for (var i = 0; i < responsesQueueCapacity; i++)
+            {
+                if (this.responses[i] == response)
+                {
+                    score += 1 / Math.Pow(logScoreFactor, i);
+                }
+            }
+
+            var maxPossibleScore = (Math.Pow(1 / logScoreFactor, responsesQueueCapacity) - 1) / ((1 / logScoreFactor) - 1);
+            return score / maxPossibleScore;
+        }
+
+        private async Task<string> GetRawResponse(string prompt)
+        {
             var messageWithoutJonSnow = prompt.Replace("jon snow", string.Empty, StringComparison.InvariantCultureIgnoreCase).Replace("jon", string.Empty, StringComparison.InvariantCultureIgnoreCase);
             var messageWithJonSnow = prompt;
 
-            var knowledgeBaseResponse1 = await this.httpService.Post(
+            var knowledgeBaseTask1 = this.httpService.Post(
                 new Uri(this.configuration["KnowledgeBaseEndpoint"]),
                 $"{{\"question\":\"{messageWithJonSnow}\",\"top\":5}}",
                 "application/json",
                 new Dictionary<string, string> { { "Authorization", $"EndpointKey {this.configuration["KnowledgeBaseEndpointKey"]}" } });
 
-            var knowledgeBaseResponse2 = await this.httpService.Post(
+            var knowledgeBaseTask2 = this.httpService.Post(
                 new Uri(this.configuration["KnowledgeBaseEndpoint"]),
                 $"{{\"question\":\"{messageWithoutJonSnow}\",\"top\":5}}",
                 "application/json",
                 new Dictionary<string, string> { { "Authorization", $"EndpointKey {this.configuration["KnowledgeBaseEndpointKey"]}" } });
 
+            var apiTasks = new List<Task>
+            {
+                knowledgeBaseTask1,
+                knowledgeBaseTask2
+            };
+
+            await Task.WhenAll(apiTasks);
+            var knowledgeBaseResponse1 = knowledgeBaseTask1.Result;
+            var knowledgeBaseResponse2 = knowledgeBaseTask2.Result;
+
             if (knowledgeBaseResponse1.IsSuccessStatusCode && knowledgeBaseResponse2.IsSuccessStatusCode)
             {
                 var responseContent1 = JsonConvert.DeserializeObject<KnowledgeBaseResponse>(knowledgeBaseResponse1.Content);
                 var responseContent2 = JsonConvert.DeserializeObject<KnowledgeBaseResponse>(knowledgeBaseResponse2.Content);
-
                 var allAnswers = new List<KnowledgeBaseAnswer>(responseContent1.Answers);
                 allAnswers.AddRange(responseContent2.Answers);
                 allAnswers.Sort(new KnowledgeBaseAnswerComparerByScore());
